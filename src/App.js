e645515bb0b4d2e0f288db3d3ac93b3605207c5f
Andrzej Kruk <endrjurejwen@gmail.com>
2019-01-11 11:49:18 +0100

    Add simple Navigation Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Navigation from './Navigation/Navigation';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navigation />
         <h1>Hello from the other side!!</h1>
       </div>
     );
---
+++ src/Navigation/Navigation.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const navigation = () => (
+  <div>Logo, Searchbar, NavigationItems, MenuButton</div>
+);
+
+export default navigation;
