e8ed918679687728fa16b790c0d26d453c5d19df
Andrzej Kruk <endrjurejwen@gmail.com>
2019-01-11 11:17:28 +0100

    change App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Hello from the other side
+      </div>
+    );
+  }
+}
+
+export default App;
