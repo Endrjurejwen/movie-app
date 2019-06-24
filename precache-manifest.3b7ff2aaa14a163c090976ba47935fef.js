0afbb482dd79282cc10c1099c9c8ceb2bc140029
Andrzej Kruk <endrjurejwen@gmail.com>
2019-06-24 13:00:24 +0200

    Updates

---
+++ precache-manifest.3b7ff2aaa14a163c090976ba47935fef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "eafc9112d7a87d4e4850db6d2fa9ee14",
+    "url": "/movie-app/static/media/logo-white.eafc9112.png"
+  },
+  {
+    "revision": "cde728466ccba9c133c1f24b359866ab",
+    "url": "/movie-app/static/media/clipart584687b.cde72846.png"
+  },
+  {
+    "revision": "2a4a27000ba76122af42",
+    "url": "/movie-app/static/js/runtime~main.2a4a2700.js"
+  },
+  {
+    "revision": "c0a13bce62176239e5cb",
+    "url": "/movie-app/static/js/main.c0a13bce.chunk.js"
+  },
+  {
+    "revision": "2435f10f7086d131d0a8",
+    "url": "/movie-app/static/js/1.2435f10f.chunk.js"
+  },
+  {
+    "revision": "1780f68dc5da0f68b0c3a3a7a7dbe165",
+    "url": "/movie-app/index.html"
+  }
+];
